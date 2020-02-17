9df7ba06b3d1ea758443cc7acb0f581cd487d035
S-Jin <skku7714@naver.com>
2020-02-17 22:22:54 +0900

    wsgi.py restored as original

--- django_project/wsgi.py
+++ django_project/wsgi.py
@@ -7,17 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
-# import os
-#
-# from django.core.wsgi import get_wsgi_application
-#
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.settings')
-#
-# application = get_wsgi_application()
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "xxxx.settings")
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.settings')
+
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
