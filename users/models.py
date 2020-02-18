c50971da14d3a9b23c1fa400df4d5b568c2178fd
S-Jin <skku7714@naver.com>
2020-02-18 22:22:23 +0900

    user/models.py is changed

--- users/models.py
+++ users/models.py
@@ -6,9 +6,9 @@ from PIL import Image
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
-    #
-    # def __str__(self):
-    #     return f'{self.user.username} Profile'
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
 
     # def save(self):
     #     super().save(*args, **kwargs)
