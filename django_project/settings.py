d782589c5a4fc994b0201d2104468251ac8cb6c2
S-Jin <skku7714@naver.com>
2020-02-18 23:09:15 +0900

    REGION changed in settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -154,7 +154,7 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
-AWS_S3_REGION_NAME = "ap-south-1"
+AWS_S3_REGION_NAME = "ap-northeast-2"
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
