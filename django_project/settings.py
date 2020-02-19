43651fb91ec6c6c7667514e9a5072dc39d820f3e
S-Jin <skku7714@naver.com>
2020-02-19 22:34:32 +0900

    AWS.txt added and settings.py changed

--- django_project/settings.py
+++ django_project/settings.py
@@ -154,7 +154,8 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
-AWS_S3_REGION_NAME = "ap-northeast-2"
+AWS_S3_REGION_NAME = "ap-northeast-2" # added to remove error about : AWS4-HMAC-SHA256
+
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
