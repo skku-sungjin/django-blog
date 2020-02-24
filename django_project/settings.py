8c5411fd035bb1bd6b67f1af5223f0f46fc33ff1
skku-sungjin <skku7714@gmail.com>
2020-02-24 23:21:07 +0900

    change sqlite to mysql (set schema named 'django-blog')

--- django_project/settings.py
+++ django_project/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django-blog',
+        'USER': 'root',
+        'PASSWORD': 'tjdwls14',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
